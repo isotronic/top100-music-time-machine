22928b70a3d3624c43dbdbba4467d0422d60fb6b
Joseph Bouqdib <joseph@bouqdib.com>
2023-06-28 09:53:55 +0200

    added feedback for the user

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ SCOPE = "playlist-modify-private"
 
 # ask user which date they want to create a playlist for and fetch the corresponding page on billboard.com
 chosen_date = input("Which date do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-chosen_year = chosen_date.split("-")
 
 response = requests.get(f"{BILLBOARD_URL}{chosen_date}")
 web_page = response.text
@@ -24,6 +23,7 @@ title_tags = soup.select(selector="li h3", class_="c-title")
 song_titles = [title.get_text().strip() for title in title_tags][:100]
 artist_names = [name.next_element.next_element.next_element.text.strip() for name in title_tags][:100]
 song_and_artist = dict(zip(song_titles, artist_names))
+print(f"Top 100 Billboard songs for {chosen_date}, were added to the list.")
 
 # authenticate the user on spotify
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID,
@@ -36,6 +36,7 @@ user_id = sp.current_user()["id"]
 
 # find all the spotify song uri's from the previously created dictionary and add them to a list.
 # if they don't exist, then skip them
+print("Searching for songs on Spotify...")
 track_uris = []
 for (song, artist) in song_and_artist.items():
     try:
@@ -44,8 +45,10 @@ for (song, artist) in song_and_artist.items():
         track_uris.append(uri)
     except IndexError:
         print("Song not found on Spotify. Skipped.")
+print(f"Found {len(track_uris)} songs on Spotify.")
 
 # create a playlist for the current user on spotify and add each track that was found to that playlist
 playlist = sp.user_playlist_create(user=user_id, name=f"{chosen_date} Billboard Top 100", public=False)
 sp.playlist_add_items(playlist_id=playlist["id"], items=track_uris)
+print("Playlist was successfully created and the songs added to it.")
 
